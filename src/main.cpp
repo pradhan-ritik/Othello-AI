448beaabe90bc2d6a8d3e54e8e05201a33cac22f
rp <pradhanr@mailfence.com>
2026-03-13 16:36:22 -0700

    Testing new board functions

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,8 @@
 int main() {
     Board b = Board(STARTING_BOARD);
     b.print_board();
+    std::cout << b.is_white(d4) << std::endl;
+    std::cout << b.is_black(d5) << std::endl;
+    std::cout << b.is_empty(d6) << std::endl;
     return 0;
 }
